c5797268bb8878decd232787fc06b0ad81e08dbe
renukanaidu365-boop <renukanaidu365@gmail.com>
2026-03-19 16:43:17 +0530

    Delete 0448-find-all-numbers-disappeared-in-an-array directory

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        a=set(nums)
-        f=[]
-        for i in range(1,n+1):
-            if i not in a:
-                f.append(i)
-        return f
