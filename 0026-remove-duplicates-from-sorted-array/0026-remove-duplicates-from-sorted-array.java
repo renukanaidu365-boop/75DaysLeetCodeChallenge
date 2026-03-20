4eee950ea6c3c6f3637f2f0ec7d894c47805fa47
renukanaidu365-boop <renukanaidu365@gmail.com>
2026-03-20 10:19:45 +0530

    Time: 1 ms (78.89%), Space: 46.8 MB (37.28%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int p = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[p] = nums[i];
+                p = p + 1;  
+            }
+        }
+        return p;  
+    }
+}
