5c5a192bab3ae8063f963566b7eadb3b16b9b727
renukanaidu365-boop <renukanaidu365@gmail.com>
2026-05-11 21:13:10 +0530

    Time: 3 ms (82.68%), Space: 20.4 MB (89.12%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,7 +3,7 @@ class Solution:
         
         l=0
         for r in range(len(nums)):
-            if nums[r]:
+            if nums[r]!=0:
                 nums[l],nums[r]=nums[r],nums[l]
                 l+=1
         
