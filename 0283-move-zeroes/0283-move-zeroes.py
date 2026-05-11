eb320ce46d5fb02ebb378ff9e318b708d308d972
renukanaidu365-boop <renukanaidu365@gmail.com>
2026-05-11 21:11:25 +0530

    Time: 6 ms (45.25%), Space: 20.5 MB (23.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        a=[]
-        b=[]
-        r=""
-        n=len(nums)
-        for i in range(n):
-            if nums[i]==0:
-                a.append(nums[i])
-            else:
-                b.append(nums[i])
-        r=b+a
-        for i in range(n):
-            nums[i]=r[i]
-        return nums[i]
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        
+        l=0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+        
+        
