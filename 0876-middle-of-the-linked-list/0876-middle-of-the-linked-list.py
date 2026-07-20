698554b9a0a92999adfe39a2d420928fa55981db
Renuka Naidu <renukanaidu365@gmail.com>
2026-07-20 09:18:50 +0530

    Time: 0 ms (100%), Space: 17.7 MB (100%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -3,19 +3,12 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        t=head
-        length=0
-        while t:
-            length=length+1
-            t=t.next
-        mid=length//2
-        t=head
-        for _ in range(mid):
-            t=t.next
-        return t
-
-
-
+        f=head
+        s=head
+        while s!=None and s.next!=None:
+            f=f.next
+            s=s.next.next
+        return f
+        
