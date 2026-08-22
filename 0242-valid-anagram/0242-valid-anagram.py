5f41a5c9f471afb7d7b6f16433e4a05961fdcb4d
Renuka Naidu <renukanaidu365@gmail.com>
2026-08-22 11:57:45 +0530

    Time: 18 ms (19.23%), Space: 20.5 MB (10.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        g={}
-        r={}
-        for i in s:
-            g[i]=g.get(i,0)+1 
-        for i in t:
-            r[i]=r.get(i,0)+1 
-        if g==r:
+        if sorted(s)==sorted(t):
             return True
-        return False
+        else:
+            return False
