2af6b76de18ef5277a93452e1714fb4b14a420b9
Renuka Naidu <renukanaidu365@gmail.com>
2026-08-23 14:06:52 +0530

    Time: 3 ms (85.12%), Space: 21.2 MB (47.2%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,21 +1,10 @@
 
-from collections import defaultdict
-from typing import List
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-
-        result = 0
-
-        mpp = defaultdict(int)
-
-        mini = n // 2 + 1
-
-        for num in nums:
-            mpp[num] += 1
-            if mpp[num] == mini:
-                result=num
-
-        
-
-        return result
+        ca=None
+        c=0
+        for i in nums:
+            if c==0:
+                ca=i 
+            c+=1 if ca==i else -1
+        return ca
